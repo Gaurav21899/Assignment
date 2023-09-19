761e9ed2b4bcedacbfadd3f753326b7ce8c3bd80
Gaurav21899 <gaurav21899@gmail.com>
2023-09-19 22:08:16 +0530

    Create a 500-meter buffer using the Python

--- Q3_A.py
+++ Q3_A.py
@@ -2,7 +2,7 @@ import arcpy
 
 # Setting the default workspace environment
 arcpy.env.workspace = r"D:\Sem-3\Programming for GIS-3\Assignment\Practical_1_ProProject\01_Running_Python_Scripts.gdb"
-arcpy.analysis.Buffer("Wilson_Schools","Wilson_Schools_Buffer_600","600 meters")
+arcpy.analysis.Buffer("Wilson_Schools","Wilson_Schools_Buffer_500","500 meters")
 
 print("Process Completed")
 
